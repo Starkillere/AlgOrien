7f169c00262d6aeae204d3310e925a8b369e4e09
Ayouba Anrezki <anrezkia531@gmail.com>
2023-04-18 23:10:01 +0200

    Définition de fonction et des route par Anrezki

--- AlgOrienApp/data.py
+++ AlgOrienApp/data.py
@@ -3,3 +3,164 @@
 class UserData:
     def __init__(self) -> None:
         pass
+def formatage(text:str) -> dict:
+    pass
+
+def find_orientation(user_data:list) -> dict:
+    pass
+
+question_catégories = {
+        "1. Quel type de travail souhaitez-vous faire ?":
+        ["a) Un travail manuel",
+        "b) Un travail de bureau",
+        "c) Un travail créatif",
+        "d) Un travail technologique"],
+        "2. Quel est votre niveau d'éducation ?":
+        ["a) Diplôme d'études secondaires",
+        "b) Diplôme universitaire",
+        "c) Certificat d'études spécialisées",
+        "d) Aucun diplôme"],
+        "3. Quel type de responsabilité souhaitez-vous avoir ?":
+        ["a) Des responsabilités importantes",
+        "b) Des responsabilités limitées",
+        "c) Aucune responsabilité",
+        "d) Des responsabilités occasionnelles"],
+        "4. Quelle est la taille de l'entreprise qui vous intéresse ?":
+        ["a) Grande entreprise",
+        "b) Petite entreprise",
+        "c) Entreprise individuelle",
+        "d) Plusieurs entreprises"],
+        "5. Quel est le cadre de travail qui vous intéresse ?":
+        ["a) En équipe",
+        "b) Seul",
+        "c) En ligne",
+        "d) En personne"],
+        "6. Quel type de salaire espérez-vous ?":
+        ["a) Un salaire régulier",
+        "b) Un salaire à commission",
+        "c) Un salaire à l'heure",
+        "d) Un salaire fixe"],
+        "7. Quel type d'environnement de travail vous intéresse ?":
+        ["a) Environnement dynamique",
+        "b) Environnement calme",
+        "c) Environnement stimulant",
+        "d) Environnement flexible"],
+        "8. Quel type de bénéfices supplémentaires recherchez-vous ?":
+        ["a) Des vacances payées",
+        "b) Des primes de rendement",
+        "c) Des avantages sociaux",
+        "d) Des options d'achat d'actions"],
+        "9. Quel est le type de carrière que vous souhaitez ?":
+        ["a) Une carrière ascendante",
+        "b) Une carrière stable",
+        "c) Une carrière variée",
+        "d) Une carrière à long terme"],
+        "10. Quel type de secteur souhaitez-vous rejoindre ?":
+        ["a) Secteur public",
+        "b) Secteur privé",
+        "c) Secteur humanitaire",
+        "d) Secteur entrepreneurial"]
+}
+
+question_metier = {
+        "1. Quel type de travail vous intéresse le plus ?":
+        ["a. Les métiers manuels",
+        "b. Les métiers à distance",
+        "c. Les métiers artistiques",
+        "d. Les métiers auxiliaires"],
+        "2. Quel est votre style de communication préféré ?":
+        ["a. Verbal",
+        "b. Écrit",
+        "c. Visuel",
+        "d. Tâches à effectuer"],
+        "3. Quel type de métier vous intéresse le plus ?":
+        ["a. Les métiers de service",
+        "b. Les métiers techniques",
+        "c. Les métiers commerciaux",
+        "d. Les métiers de l'informatique"],
+        "4. Quel est votre niveau d'études ?":
+        ["a. Diplomé d'un niveau d'études supérieur",
+        "b. Diplomé d'un niveau d'études intermédiaire",
+        "c. Diplomé d'un niveau d'études secondaire",
+        "d. Autodidacte"],
+        "5. Quel type d'environnement de travail vous convient le mieux ?":
+        ["a. Un environnement en équipe",
+        "b. Un environnement autonome",
+        "c. Un environnement stimulant",
+        "d. Un environnement flexible"],
+        "6. Quel type de salaire souhaitez-vous ?":
+        ["a. Un salaire fixe et élevé",
+        "b. Un salaire évolutif",
+        "c. Un salaire bas mais avec des primes",
+        "d. Un salaire avec des avantages en nature"],
+        "7. Quel type de métiers vous intéresse le plus ?":
+        ["a. Les métiers de l'enseignement",
+        "b. Les métiers de la finance",
+        "c. Les métiers de l'informatique",
+        "d. Les métiers du divertissement"],
+        "8. Quel type d'emploi souhaitez-vous ?":
+        ["a. Un emploi à temps plein",
+        "b. Un emploi à temps partiel",
+        "c. Un emploi à domicile",
+        "d. Un emploi à l'étranger"],
+        "9. Quel type de métiers vous intéresse le plus ?":
+        ["a. Les métiers du secteur public",
+        "b. Les métiers du secteur privé",
+        "c. Les métiers de l'Humanitaire",
+        "d. Les métiers de l'entrepreneuriat"],
+        "10. Quelle est votre expérience professionnelle la plus marquante ?":
+        ["a. Une mission réussie",
+        "b. Une promotion",
+        "c. Une reconversion professionnelle",
+        "d. Un projet innovant"],
+        "11. Quel type d'ambiance de travail vous convient le mieux ?":
+        ["a. Une ambiance dynamique",
+        "b. Une ambiance calme",
+        "c. Une ambiance stimulante",
+        "d. Une ambiance collaborative"],
+        "12. Quels sont les facteurs qui motivent le plus votre choix d'un métier ?":
+        ["a. Le salaire",
+        "b. La flexibilité",
+        "c. L'autonomie",
+        "d. La reconnaissance"],
+        "13. Quel type de métiers vous intéresse le plus ?":
+        ["a. Les métiers de l'ingénierie",
+        "b. Les métiers de la santé",
+        "c. Les métiers de l'éducation",
+        "d. Les métiers du commerce"],
+        "14. Quel type d'organisation préférez-vous ?":
+        ["a. Une organisation hiérarchisée",
+        "b. Une organisation horizontale",
+        "c. Une organisation à objectifs",
+        "d. Une organisation à mission"],
+        "15. Quel type de management vous convient le mieux ?":
+        ["a. Un management par objectifs",
+        "b. Un management par projet",
+        "c. Un management par la qualité",
+        "d. Un management par la formation"],
+        "16. Quel type de métiers vous intéresse le plus ?":
+        ["a. Les métiers de la technologie",
+        "b. Les métiers des médias",
+        "c. Les métiers du marketing",
+        "d. Les métiers de la vente"],
+        "17. Quel type de personnes appréciez-vous le plus ?":
+        ["a. Les personnes créatives",
+        "b. Les personnes communicatives",
+        "c. Les personnes ambitieuses",
+        "d. Les personnes logiques"],
+        "18. Quel type de métiers vous intéresse le plus ?":
+        ["a. Les métiers de l'architecture",
+        "b. Les métiers de la gestion",
+        "c. Les métiers de l'informatique",
+        "d. Les métiers de l'écriture"],
+        "19. Quel type de tâches vous intéresse le plus ?":
+        ["a. Les tâches analytiques",
+        "b. Les tâches créatives",
+        "c. Les tâches organisationnelles",
+        "d. Les tâches administratives"],
+        "20. Quel type de travail vous intéresse le plus ?":
+        ["a. Le travail en équipe",
+        "b. Le travail autonome",
+        "c. Le travail à domicile",
+        "d. Le travail à distance"]
+        }
--- AlgOrienApp/views.py
+++ AlgOrienApp/views.py
@@ -10,3 +10,14 @@ app = Flask(__name__)
 app.config.from_object("config")
 app.secret_key = app.config["SECRET_KEY"]
 app.permanent_session_lifetime = timedelta(days=5)
+
+@app.route('/')
+def acceuil():
+    return render_template("accueil.html")
+
+@app.route("/questionnaire", methods=["GET", "POST"])
+def questionnaire():
+    if request.method == "POST":
+        return render_template("resulta.html")
+    else:
+        return render_template("questionnaire.html")
